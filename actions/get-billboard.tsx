c41f0c3258f3b4ae11580fa17cf01410b6208163
Agung Budie Pangestu <agungbudiepangestu@gmail.com>
2024-05-03 17:01:04 +0700

    create category page

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -4,8 +4,6 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 const getBillboard = async (id: string): Promise<Billboard> => {
     const res = await fetch(`${URL}/${id}`);
 
-    console.log((`${URL}/${id}`));
-
     return res.json();
 }
 
--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -4,8 +4,6 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 const getProduct = async (id: string): Promise<Product> => {
     const res = await fetch(`${URL}/${id}`);
 
-    console.log((`${URL}/${id}`));
-
     return res.json();
 }
 
